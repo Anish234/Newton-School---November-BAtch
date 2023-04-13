0a95aa0cf4e1ec8d35c5144db1920689610deb69
Anish <anishsrt4@gmail.com>
2023-04-14 01:02:40 +0530

    09/04/23

---
+++ JavaScript_Contest/Primitives.js
@@ -0,0 +1,20 @@
+// Primitives
+// easy
+// Problem Statement
+// Initialize two variables name and age with values of data types string and number respectively.
+// Create another variable greet with the value of "My name is name and I am age years old" and print the value of greet in the console.
+
+
+// Note: Generate Expected Output section will not work for this question
+// DO NOT CONSOLE.LOG the variables, just declare them.
+// Input
+// There is no input required for this question
+// Output
+// There is no output required for this question
+
+// your code is here
+
+let name = "Anish";
+let age = 28;
+let greet = "My name is Anish and I am 28 years old";
+// console.log(greet);
