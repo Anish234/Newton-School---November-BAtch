7aa1a3f41d74db5e70cdf801ba40449618f91e27
Anish <anishsrt4@gmail.com>
2023-04-14 01:30:55 +0530

    javascript

---
+++ JavaScript_Assignment/JS_Primitive_Types.js
@@ -0,0 +1,27 @@
+// JS Primitive Types
+// easy
+// Problem Statement
+// Create 6 variables with namesvarA, varB, varC, varD, varE, varF
+// Store 6 different values of your choice in the variables such that the variables represent the given primitive types in the given order:
+// varA: Number
+// varB: String
+// varC: Boolean
+// varD: Undefined
+// varE: Object
+// varF: Symbol
+// Input
+// No input is required, you just need to create the variables.
+// Output
+// .No output is required for this question.
+
+
+
+
+// your code is here
+
+var varA = 5;
+var varB = "Hello World";
+var varC = true;
+var varD = undefined;
+var varE = {name: "John"};
+var varF = Symbol('abc');
